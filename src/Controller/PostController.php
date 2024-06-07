82ca68fef0306d65b670a6475e79953d58d075a5
rem <remi.arnal@etu.uca.fr>
2024-06-07 15:02:16 +0200

    moving nested folders + postcontroller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,17 +1,63 @@
+<?php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
+use Symfony\Component\Serializer\Serializer;
 
-class PostController
-{
-    #[Route('/lucky/number/{max}', name: 'app_lucky_number')]
-    public function number(int $max): Response
+class PostController {
+
+    private EntityManagerInterface $em;
+    private Serializer $serializer;
+
+    public function __construct(EntityManagerInterface $em, Serializer $serializer)
     {
-        $number = random_int(0, $max);
+        $this->em = $em;
+        $this->serializer = $serializer;
+    }
+    
+    #[Route('/post/{id}', name: 'display post', methods: ['GET'])]
+    public function getPost(int $id): Response
+    {
+        $post = $this->em->getRepository(Post::class)->find($id);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
-    }	
+        if(!$post) {
+            # Error 404 page
+        }
+
+        # Return twig
+        return new Response();
+    }
+
+    #[Route('/post/', name: 'add_post', methods: ['POST'])]
+    public function addPost(Request $request) :Response
+    {
+        $data = json_decode($request->getContent(), true);
+        
+        try {
+            $post = $this->serializer->deserialize($data, Post::class, 'json');
+        } catch (\Exception) {
+            return new Response("Invalid JSON data", Response::HTTP_BAD_REQUEST);
+        }
+
+        # Handle error on data
+        $this->em->persist($post);
+        $this->em->flush();
+                
+        return new Response();
+    }
+
+    #[Route('/post/{id}', name: 'remove_post', methods: ['DELETE'])]
+    public function removePost(int $id) :Response
+    {
+        $postRef = $this->em->getReference('Post', $id);
+        $this->em->remove($postRef);
+        $this->em->flush();
+
+        return new Response();
+    }
 }
