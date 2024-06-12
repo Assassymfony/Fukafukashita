8c89c8c8a12dba55340dc3c65d088e022d241762
Corentin R <corentin.richard@etu.uca.fr>
2024-06-12 17:26:03 +0200

    Ajout du formulaire de modification des profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Profil;
+use App\Form\ProfilType;
 use Doctrine\ORM\EntityManager;
+use SebastianBergmann\Environment\Console;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProfilController extends AbstractController
 {
@@ -19,13 +22,17 @@ class ProfilController extends AbstractController
     #[Route('/profil/{id}',name:'profil_show', requirements: ['page' => '\d+'])]
     public function profil(int $id): Response
     {
+        $connected = $this->isGranted('ROLE_USER');
+        // $connected = $this->isGranted('ROLE_USER') != false;
+
         $profil = $this->mgr->find(Profil::class, $id);
         $posts = $profil->getPosts();
         return $this->render('profil/index.html.twig', [
             'profil' => $profil,
             'posts' => $posts,
             'followFlag' => $profil->getFollowers()->contains($this->getUser()),
-            'selfFlag' => $profil != $this->getUser()
+            'selfFlag' => $profil == $this->getUser(),
+            'connected' => $connected
         ]);
     }
 
@@ -83,14 +90,53 @@ class ProfilController extends AbstractController
             ]);
     }
 
-    #[Route('/profil/new', name: 'profil_new')]
-    public function new(): Response
-    {
-        $profil = new Profil();
+    // #[Route('/profil/new', name: 'profil_new')]
+    // public function new(): Response
+    // {
+    //     $profil = new Profil();
 
-        return $this->redirectToRoute('profil_show', ['id' => $profil->getId()]);
+    //     return $this->redirectToRoute('profil_show', ['id' => $profil->getId()]);
+    // }
+
+    #[Route('/profil/{id}/edit',name:'profil_edit', requirements: ['page'=> '\d'])]
+    public function editProfil(int $id,Request $request): Response
+    {
+        $profil = $this->mgr->find(Profil::class, $id);
+
+        $form = $this->createForm(ProfilType::class, $profil);
+
+        $form->handleRequest($request);
+        if( $form->isSubmitted() && $form->isValid() ) {
+            $form->getData();
+            $this->mgr->persist($profil);
+            $this->mgr->flush();
+
+            return $this->redirectToRoute('profil_show', ['id' => $id]);
+        }
+    
+        return $this->render('profil/edit.html.twig', [
+            'form'=> $form,
+            'profil' => $profil,
+            ]);
     }
 
+    #[Route('/profil/{id}/delete', name: 'profil_delete', methods: ['POST'], requirements: ['id' => '\d+'])]
+    public function delete(int $id, Request $request): Response
+    {
+        $profil = $this->mgr->find(Profil::class, $id);
+
+        if (!$profil) {
+            throw $this->createNotFoundException('The profile does not exist');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$profil->getId(), $request->request->get('_token'))) {
+            $this->mgr->remove($profil);
+            $this->mgr->flush();
+            $this->addFlash('success', 'Profile deleted successfully');
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
 
 
 }
