8b0ed1c7c6e1d8182e5fa53a1864087c8bccdd9c
rem <remi.arnal@etu.uca.fr>
2024-06-14 11:24:52 +0200

    improve ui for login and register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,28 +6,32 @@ use App\Entity\Profil;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'invalid_message' => 'You should agree to our terms.',
+            ->add('name', TextType::class, [
+                'label' => 'Username'
             ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Password',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'invalid_message' => 'Please enter a password',
             ])
-        ;
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'invalid_message' => 'You should agree to our terms.',
+            ])
+            ->add('submit', SubmitType::class, [
+                    'label' => 'Register'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
